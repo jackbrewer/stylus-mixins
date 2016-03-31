870633480f6980b60e0f971b2c920dc3edf04ad9
Jack Brewer <jack@jackbrewer.co.uk>
2016-03-31 17:03:01 +0100

    Adds eslint and fixes linting issues

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,10 +1,10 @@
-var testRunnerConfig = {
-  describe: 'Stylus Mixins',
-  testDirPath: './test',
-  stylus: {
-    use: require('../lib/stylus-mixins')(),
-    import: '../index.styl'
+var testRunnerConfig =
+  { describe: 'Stylus Mixins'
+  , testDirPath: './test'
+  , stylus:
+    { use: require('../lib/stylus-mixins')()
+    , import: '../index.styl'
+    }
   }
-}
 
 require('stylus-test-runner')(testRunnerConfig)
